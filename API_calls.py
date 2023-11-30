abce39f0371eab677cac25193fa9ff0a8640232c
thorben <145192476+Code2703@users.noreply.github.com>
2023-11-30 17:39:00 +0100

    re-included old rooms function

--- API_calls.py
+++ API_calls.py
@@ -127,7 +127,6 @@ class API:
 
         return rooms_df
     
-
     # def next_event(self, df, room_nr, filter_end, room_info):
     def next_event(self, df, room_nr, filter_end):
         room_events = df.query("room_nr == @room_nr and start_time.dt.time > @filter_end")
@@ -195,3 +194,51 @@ class API:
         excluded_buildingName = ['A 10 Sporthalle', 'D 14 WBZ Holzweid', 'D 15 Hans Ulrich Auditorium', 'A 11 Square']
         result_df = rooms_df.query("name in @included_name and not (infoUrlText in @excluded_infoUrlText or buildingName in @excluded_buildingName)").copy()
         return result_df
+    
+    def old_rooms(self):
+        """Returns a pandas dataframe containing all campus rooms of format xx-(U)xxx, including their capacity and system IDs. Note that some rooms have multiple IDs."""
+        # API URL for Rooms
+        url = "https://integration.preprod.unisg.ch/toolapi/Rooms"
+
+        # Headers for API call
+        headers = {
+            "X-ApplicationId": self.api_token,
+            "API-Version": "1",
+            "X-RequestedLanguage": "en"
+        }
+
+        # Get response from API
+        response = requests.get(url, headers=headers)
+
+        # Error handling for API call
+        if response.ok:
+            json_response = response.json()
+            print(json_response)
+            df = pd.DataFrame(json_response)
+        else:
+            print("Error: ", response.status_code)
+
+        # Define RegEx pattern to look for on-campus rooms (pattern: xx-(U)xxx)
+        pattern = r"\b\d{2}-[U]?\d{3}\b"
+
+        # Boolean mask based on specified RegEx pattern
+        mask = df['shortName'].str.contains(pattern, na=False)
+
+        # Filter rooms using RegEx mask
+        campus_rooms = df[mask].copy()
+
+        # Extract clean room number from shortName column
+        campus_rooms['shortName_clean'] = campus_rooms['shortName'].apply(lambda x: re.split(r"[\s/]", x)[0].strip("#"))
+
+        # Second mask to ensure consistency
+        mask = campus_rooms['shortName_clean'].str.contains(pattern, na=False)
+        campus_rooms = campus_rooms[mask]
+
+        # Group by shortName as unique identifier and collect all IDs assigned in the system, seating capacity and floor
+        campus_rooms_clean = campus_rooms.groupby('shortName_clean')[['id', 'floor', 'seats']].agg(list).reset_index()
+
+        # Keep only one of the (identical) floor values and only the largest capacity value
+        campus_rooms_clean['floor'] = campus_rooms_clean['floor'].apply(lambda x: x[0])
+        campus_rooms_clean['seats'] = campus_rooms_clean['seats'].apply(lambda x: max(x))
+
+        return campus_rooms_clean
